2fbb4222d0d4bf531ab7e37c0c075ae2eda18df7
valibus <pvalibus@gmail.com>
2014-10-31 17:59:54 +0100

    maj files

---
+++ Code_Source/app/code/local/Valibus/Fbreview/Block/Count.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: pvalibus
+ * Date: 31/10/14
+ * Time: 17:18
+ */
+class Valibus_Fbreview_Block_Count extends Mage_Review_Block_Helper {
+    public function getReviewsCount(){
+        return 42;
+    }
+}
--- Code_Source/app/code/local/Valibus/Fbreview/Helper/Data.php
+++ Code_Source/app/code/local/Valibus/Fbreview/Helper/Data.php
@@ -5,5 +5,4 @@
  * Time: 16:38
  */
 class Valibus_Fbreview_Helper_Data extends Mage_Core_Helper_Abstract {
-
 }
