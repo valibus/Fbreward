08a12539ba5a9cf17b729eb5419be13425767516
valibus <pvalibus@gmail.com>
2014-11-18 17:24:14 +0100

    maj code

--- Code_Source/app/code/local/Valibus/Fbreward/Helper/Data.php
+++ Code_Source/app/code/local/Valibus/Fbreward/Helper/Data.php
@@ -1,9 +1,20 @@
 <?php
-/**
- * User: pvalibus
- * Date: 31/10/14
- * Time: 16:38
- */
-class Valibus_Fbreview_Helper_Data extends Mage_Core_Helper_Abstract {
+/*
+ * @author Pvalibus
+ * @description Fbreward class */
 
+class Valibus_Fbreward_Helper_Data extends Mage_Core_Helper_Abstract{
+    const FBREWARD_SHAREPAGE_RULE_ID = 12;
+    public function getGeneratedCouponLabel(){
+        return $this->getGeneratedCoupon()->code;
+
+    }
+    private function getGeneratedCoupon(){
+        /** @var $couponCollection Mage_SalesRule_Model_Resource_Coupon_Collection */
+        $couponCollection=mage::getModel('salesrule/coupon')
+            ->getCollection()
+            ->addFieldToFilter('rule_id',$this::FBREWARD_SHAREPAGE_RULE_ID)
+            ->getLastItem();
+        return $couponCollection;
+    }
 }
